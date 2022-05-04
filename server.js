5e47ce0b125a93274e02fa1567783ccfa67651d9
Tritonn204 <tritonn204@gmail.com>
2022-05-04 06:02:03 -0500

    cors fix

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const require = createRequire(import.meta.url);
 const express = require('express');
 var cors = require('cors');
 var expressApp = express();
-expressApp.use(cors());
+expressApp.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 const http = require('http');
 const port = parseInt(process.env.PORT) || 4000;
