0342f7d18d0b4a7b091ea8869244c54a9c4552b2
Tritonn204 <tritonn204@gmail.com>
2022-05-06 03:55:14 -0500

    wallet switch tolerance

--- server.js
+++ server.js
@@ -139,6 +139,7 @@ io.on("connection", socket => {
   socket.userData = {};
 
   socket.on('setWallet', async (data) => {
+    if (socket.userData == undefined) socket.userData = {};
     let sig = ethers.utils.splitSignature(data.signature);
     let recovered = await verifier.verifyString(data.message, sig.v, sig.r, sig.s);
     socket.userData.wallet = recovered.toLowerCase();
